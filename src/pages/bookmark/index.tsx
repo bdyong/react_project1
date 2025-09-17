f76483c17d27fac90ed2a14d17cf72359265a602
bdyong <qkdeodyd00@naver.com>
2025-09-17 22:53:55 +0900

    Add files via upload

---
+++ src/pages/bookmark/index.tsx
@@ -0,0 +1,8 @@
+// src/pages/bookmark/index.tsx
+
+function BookmarkPage() {
+  // ... 컴포넌트 코드 ...
+  return <div>북마크 페이지</div>;
+}
+
+export default BookmarkPage; // 이 줄을 추가하세요
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles/index.module.scss"
+
+function index() {
+  return (
+    <div className={styles.page}>
+        {/* 공통 헤더 ui 부분*/}
+        {/* 공통 네비게이션 ui 부분*/}
+        <div className={styles.page_contents}>
+            <div className={styles.page_contents_introBox}>
+                <div className={styles.wrapper}>
+                    <span className={styles.wrapper_title}>PhtoSplash</span>
+                    <span className={styles.wrapper_desc}>
+                        인터넷의 시각 자료 출처입니다. <br />
+                        모든 지역에 있는 크리에이터들의 지원을 받습니다.
+                        </span>
+                        {}
+                </div>
+            </div>
+            <div className={styles.page_contents_imageBox}></div>
+        </div>
+            {/* 공통 푸터 ui 부분*/}
+    </div>
+  )
+}
+
+export default index
